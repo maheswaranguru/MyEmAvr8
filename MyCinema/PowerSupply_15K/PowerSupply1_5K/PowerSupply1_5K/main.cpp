9505ff24c0b24c1cf8e38d60c2b7227b558699e9
mahesh <maaply@gmail.com>
2018-10-16 06:24:22 +0530

    Initial Version.

---
+++ MyCinema/PowerSupply_15K/PowerSupply1_5K/PowerSupply1_5K/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * PowerSupply1_5K.cpp
+ *
+ * Created: 16-10-2018 06:13:07
+ * Author : maaply
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+        
+    }
+}
+
